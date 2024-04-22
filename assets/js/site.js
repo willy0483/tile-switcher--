3dddd598d70fccb954012cef6e31aadf154ba24f
willy0483 <willyjensen251@gmail.com>
2024-04-22 13:13:04 +0200

    add tile switcher

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,47 @@
 // write cool JS hwere!!
+let mainImage = document.getElementById('mainImage');
+let smallImgOne = document.getElementById('smallImgOne');
+let smallImgTwo = document.getElementById('smallImgTwo');
+let smallImgThree = document.getElementById('smallImgThree');
+let smallImgFour = document.getElementById('smallImgFour');
+
+smallImgOne.addEventListener('click', function (envet){
+
+   let bigImageUrl = mainImage.src
+   let smallImgUrl = smallImgOne.src
+
+
+   mainImage.src = smallImgUrl
+   smallImgOne.src = bigImageUrl
+    
+})
+smallImgTwo.addEventListener('click', function (envet){
+
+    let bigImageUrl = mainImage.src
+    let smallImgUrl = smallImgTwo.src
+ 
+ 
+    mainImage.src = smallImgUrl
+    smallImgTwo.src = bigImageUrl
+     
+ })
+ smallImgThree.addEventListener('click', function (envet){
+
+    let bigImageUrl = mainImage.src
+    let smallImgUrl = smallImgThree.src
+ 
+ 
+    mainImage.src = smallImgUrl
+    smallImgThree.src = bigImageUrl
+     
+ })
+ smallImgFour.addEventListener('click', function (envet){
+
+    let bigImageUrl = mainImage.src
+    let smallImgUrl = smallImgFour.src
+ 
+ 
+    mainImage.src = smallImgUrl
+    smallImgFour.src = bigImageUrl
+     
+ })
