e73655cf4fd8185f45a8e43abbe3e4c18f425d35
willy0483 <willyjensen251@gmail.com>
2024-04-23 10:18:58 +0200

    Update site.js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,47 +1,27 @@
 // write cool JS hwere!!
-let mainImage = document.getElementById('mainImage');
-let smallImgOne = document.getElementById('smallImgOne');
-let smallImgTwo = document.getElementById('smallImgTwo');
-let smallImgThree = document.getElementById('smallImgThree');
-let smallImgFour = document.getElementById('smallImgFour');
+let mainImage = document.getElementById("mainImage");
+let smallImgOne = document.getElementById("smallImgOne");
+let smallImgTwo = document.getElementById("smallImgTwo");
+let smallImgThree = document.getElementById("smallImgThree");
+let smallImgFour = document.getElementById("smallImgFour");
 
-smallImgOne.addEventListener('click',  (e) => {
-
-   let bigImageUrl = mainImage.src
-   let smallImgUrl = smallImgOne.src
-
-
-   mainImage.src = smallImgUrl
-   smallImgOne.src = bigImageUrl
-    
+smallImgOne.addEventListener("click", (e) => {
+  ChangeImage(smallImgOne)
 })
-smallImgTwo.addEventListener('click', (e) => {
+smallImgTwo.addEventListener("click", (e) => {
+  ChangeImage(smallImgTwo)
+})
+smallImgThree.addEventListener("click", (e) => {
+  ChangeImage(smallImgThree)
+})
+smallImgFour.addEventListener("click", (e) => {
+  ChangeImage(smallImgFour)
+})
 
-    let bigImageUrl = mainImage.src
-    let smallImgUrl = smallImgTwo.src
- 
- 
-    mainImage.src = smallImgUrl
-    smallImgTwo.src = bigImageUrl
-     
- })
- smallImgThree.addEventListener('click', (e) => {
+function ChangeImage(smallImageRecived) {
 
-    let bigImageUrl = mainImage.src
-    let smallImgUrl = smallImgThree.src
- 
- 
-    mainImage.src = smallImgUrl
-    smallImgThree.src = bigImageUrl
-     
- })
- smallImgFour.addEventListener('click', (e) => {
+  let bigImageUrl = mainImage.src;
 
-    let bigImageUrl = mainImage.src
-    let smallImgUrl = smallImgFour.src
- 
- 
-    mainImage.src = smallImgUrl
-    smallImgFour.src = bigImageUrl
-     
- })
+  mainImage.src = smallImageRecived.src
+  smallImageRecived.src = bigImageUrl
+}
